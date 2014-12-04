5d1b8deb146c9693d4c1a525dc65fea2f9b604d2
Tom Kennedy <tkennedy@smu.edu>
2014-12-03 23:37:24 -0600

    done.

--- HW6Q4T.java
+++ HW6Q4T.java
@@ -19,43 +19,30 @@ return new String(new char[times]).replace("\0", str);
    public static void main(String[] args){
  
  Hashtable<String, Integer> frequencies = new Hashtable<String, Integer>();
-    {
+
   
  String paragraph = "I would like you all to know that I have been working on this programming thing for far too long. Java is complex and can be hard to learn; however, we continue on. I'm really just typing this so that I can create something with 100 characters. I can probably stop because I confused characters with words, and 100 characters is far fewer than 100 words.";
- //String[] paragraph = new String[] {"I would like you all to know that I have been working on this programming thing for far too long. Java is complex and can be hard to learn; however, we continue on. I'm really just typing this so that I can create something with 100 characters. I can probably stop because I confused characters with words, and 100 characters is far fewer than 100 words."};
- 
- // char[] alpha = new char[26];
+
  // int [] frequencies = new int [26];//{a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u ,v ,w, x ,y, z};
-  int index = 0;
-  char c = 'a';
-      for(int i = 0; i < 26; i++){
-      
-                frequencies.put((c+""), 0); 
-              
-                c++;
-             //   System.out.println(alpha[i]);
-            }
+  System.out.println(frequencies.toString());
 
 
-
-     for(int j = 0; j < paragraph.length() ; j++)
+  for(int j = 0; j < paragraph.length() ; j++)
   {
-     // frequencies[j] = 0;
-   //char a = paragraph.charAt(j);
-    if(paragraph.charAt(j) == ' ') continue;
-         char l = paragraph.charAt(j); 
-         int v = frequencies.get(l+"");
-          frequencies.put((l+""), v++);
-         //if(paragraph.charAt(j) == alpha[j]){ 
-         //frequencies[j]++;
-}
-System.out.println(frequencies);
-}}
-//
-        
-  
+    //gets char, converts to lower case, converts to string
+    char l = paragraph.charAt(j);
+    String g = Character.toLowerCase(l) + ""; 
 
-   
+    //inits char at 0 if not in table, stores value in old
+    frequencies.putIfAbsent(g,0);
+    int old = frequencies.get(g);
+
+    //System.out.println(old + " [" + g + "]");
+    //puts new (incremented) value in for key
+    frequencies.replace(g, ++old);
+}
+  System.out.println(frequencies.toString());
+}
 }
 
 
